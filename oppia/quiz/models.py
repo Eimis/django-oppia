0531c624f9c2913c64e520c46c5275d6b22b3a1e
Eimantas <eimantas.stonys@gmail.com>
2015-05-01 14:36:33 +0300

    Add 'feedback' field to QuizAttemptResponse model

--- oppia/quiz/models.py
+++ oppia/quiz/models.py
@@ -20,16 +20,16 @@ class Question(models.Model):
     owner = models.ForeignKey(User)
     created_date = models.DateTimeField('date created',default=timezone.now)
     lastupdated_date = models.DateTimeField('date updated',default=timezone.now)
-    title = models.TextField(blank=False)  
-    type = models.CharField(max_length=15,choices=QUESTION_TYPES, default='multichoice') 
-    
+    title = models.TextField(blank=False)
+    type = models.CharField(max_length=15,choices=QUESTION_TYPES, default='multichoice')
+
     class Meta:
         verbose_name = _('Question')
         verbose_name_plural = _('Questions')
-        
+
     def __unicode__(self):
         return self.title
-    
+
     def get_maxscore(self):
         props = QuestionProps.objects.get(question=self,name='maxscore')
         return float(props.value);
@@ -42,14 +42,14 @@ class Response(models.Model):
     score = models.DecimalField(default=0,decimal_places=2, max_digits=6)
     title = models.TextField(blank=False)
     order = models.IntegerField(default=1)
-    
+
     class Meta:
         verbose_name = _('Response')
         verbose_name_plural = _('Responses')
-        
+
     def __unicode__(self):
         return self.title
-    
+
 class Quiz(models.Model):
     owner = models.ForeignKey(User)
     created_date = models.DateTimeField('date created',default=timezone.now)
@@ -59,18 +59,18 @@ class Quiz(models.Model):
     title = models.TextField(blank=False)
     description = models.TextField(blank=True)
     questions = models.ManyToManyField(Question, through='QuizQuestion')
-    
+
     class Meta:
         verbose_name = _('Quiz')
         verbose_name_plural = _('Quizzes')
-        
+
     def __unicode__(self):
         return self.title
-    
+
     def no_attempts(self):
         no_attempts = QuizAttempt.objects.filter(quiz=self).count()
         return no_attempts
-    
+
     def avg_score(self):
         # TODO - sure this could be tidied up
         attempts = QuizAttempt.objects.filter(quiz=self)
@@ -82,12 +82,12 @@ class Quiz(models.Model):
         else:
             avg_score = 0
         return avg_score
-    
+
 class QuizQuestion(models.Model):
     quiz = models.ForeignKey(Quiz)
     question = models.ForeignKey(Question)
     order = models.IntegerField(default=1)
-    
+
     class Meta:
         verbose_name = _('QuizQuestion')
         verbose_name_plural = _('QuizQuestions')
@@ -96,38 +96,38 @@ class QuizProps(models.Model):
     quiz = models.ForeignKey(Quiz)
     name = models.CharField(max_length=200)
     value = models.TextField(blank=True)
-    
+
     class Meta:
         verbose_name = _('QuizProp')
         verbose_name_plural = _('QuizProps')
-        
+
     def __unicode__(self):
         return self.name
-    
+
 class QuestionProps(models.Model):
     question = models.ForeignKey(Question)
     name = models.CharField(max_length=200)
     value = models.TextField(blank=True)
-    
+
     class Meta:
         verbose_name = _('QuestionProp')
         verbose_name_plural = _('QuestionProps')
-        
+
     def __unicode__(self):
         return self.name
-    
+
 class ResponseProps(models.Model):
     response = models.ForeignKey(Response)
     name = models.CharField(max_length=200)
     value = models.TextField(blank=True)
-    
+
     class Meta:
         verbose_name = _('ResponseProp')
         verbose_name_plural = _('ResponseProps')
-        
+
     def __unicode__(self):
         return self.name
-    
+
 class QuizAttempt(models.Model):
     user = models.ForeignKey(User)
     quiz = models.ForeignKey(Quiz)
@@ -138,54 +138,55 @@ class QuizAttempt(models.Model):
     ip = models.IPAddressField()
     instance_id = models.CharField(max_length=50,null=True,blank=True)
     agent = models.TextField(blank=True)
-    
+
     class Meta:
         verbose_name = _('QuizAttempt')
         verbose_name_plural = _('QuizAttempts')
-        
+
     def get_score_percent(self):
         if self.maxscore > 0:
             percent = int(round(self.score * 100 / self.maxscore))
         else:
             percent = 0
         return percent
-    
+
     def is_first_attempt(self):
         no_attempts = QuizAttempt.objects.filter(user=self.user,quiz=self.quiz).count()
         if no_attempts == 1:
             return True
         else:
             return False
-    
-    def is_first_attempt_today(self):    
+
+    def is_first_attempt_today(self):
         olddate = datetime.datetime.now() + datetime.timedelta(hours=-24)
         no_attempts_today = QuizAttempt.objects.filter(user=self.user,quiz=self.quiz,submitted_date__gte=olddate).count()
         if no_attempts_today == 1:
             return True
         else:
             return False
-        
+
     def get_quiz_digest(self):
         qp = QuizProps.objects.filter(quiz=self.quiz,name='digest')
         if qp.count() == 1:
             return qp[0].value
         else:
             return None
-        
+
 class QuizAttemptResponse(models.Model):
     quizattempt = models.ForeignKey(QuizAttempt)
     question = models.ForeignKey(Question)
     score = models.DecimalField(decimal_places=2, max_digits=6)
     text = models.TextField(blank=True)
-    
+    feedback = models.TextField(null=True, blank=True)
+
     class Meta:
         verbose_name = _('QuizAttemptResponse')
         verbose_name_plural = _('QuizAttemptResponses')
-       
+
     def get_score_percent(self):
         if self.question.get_maxscore() > 0:
             percent = int(round(float(self.score) * 100 / self.question.get_maxscore()))
         else:
             percent = 0
         return percent
-  
+
